e92b847502ef59efe1886d8d0fb9a6f0d69be150
HexRdesign <support@hexr.org>
2018-08-21 21:22:49 -0500

    :mute: make ghost quiet on run

--- install-ghost.sh
+++ install-ghost.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 yarn add ghost-cli --silent --no-emoji --no-progress --non-interactive
-node_modules/.bin/ghost install local -d instance --db=sqlite3 --no-prompt --quiet
-node_modules/.bin/ghost stop -d instance --no-prompt --quiet
+node_modules/.bin/ghost install local -d instance --db=sqlite3 --no-prompt
+node_modules/.bin/ghost stop -d instance --no-prompt
 # remove stdout as a transport
 sed -i 's/    "file",/    "file"/' instance/config.development.json
 sed -i 's/    "stdout"//' instance/config.development.json
-NODE_ENV="production" database__connection__filename="$(pwd)/test/fixtures/ghost-local.db" node instance/current/index.js &
+database__connection__filename="$(pwd)/test/fixtures/ghost-local.db" node instance/current/index.js &
